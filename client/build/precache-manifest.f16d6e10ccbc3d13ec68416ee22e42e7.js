b29714f9dc2f88499122b05601308c815d8cbdd0
Nicholas_Huang713 <nhuang713@gmail.com>
2020-03-02 02:22:33 -0800

    deployment second commit

---
+++ client/build/precache-manifest.f16d6e10ccbc3d13ec68416ee22e42e7.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd93ffe69dfd470db624866036961160",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd64088a300acdaadc39",
+    "url": "/static/css/main.a55eb1af.chunk.css"
+  },
+  {
+    "revision": "7ef2a1ed52f24f2e3c6b",
+    "url": "/static/js/2.43aa5491.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.43aa5491.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd64088a300acdaadc39",
+    "url": "/static/js/main.60e7ffe7.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  },
+  {
+    "revision": "7d66f8285694f8c175a744518cfa8692",
+    "url": "/static/media/70sllcool.7d66f828.jpg"
+  },
+  {
+    "revision": "b06b4c0ea14a91979f3994f1a582d7d1",
+    "url": "/static/media/80s.b06b4c0e.jpg"
+  },
+  {
+    "revision": "fe4da0a2dbd4c3ede185d2ce26b49bea",
+    "url": "/static/media/90s.fe4da0a2.jpg"
+  },
+  {
+    "revision": "38a769786a0bf7829de9b2c547892105",
+    "url": "/static/media/audience.38a76978.jpg"
+  },
+  {
+    "revision": "b65a12c34ee42ec34a63aea148d7dcc9",
+    "url": "/static/media/djblue.b65a12c3.jpg"
+  },
+  {
+    "revision": "48908e8e5ed7d9ee2c885bd076396639",
+    "url": "/static/media/hiphopcrowd1.48908e8e.jpg"
+  },
+  {
+    "revision": "cbd99399a37bbf1678a4824f202303b4",
+    "url": "/static/media/liked.cbd99399.png"
+  },
+  {
+    "revision": "0b8c8962afeaf3d80291415bd527a90e",
+    "url": "/static/media/mic.0b8c8962.jpg"
+  },
+  {
+    "revision": "a72f52e24991e11aa9768f458c02c8ba",
+    "url": "/static/media/records.a72f52e2.png"
+  },
+  {
+    "revision": "ad6c7d245cf0d0759984c588e796b19e",
+    "url": "/static/media/turntable.ad6c7d24.png"
+  },
+  {
+    "revision": "c2fa355eb07f71c1c114a3bce5931af7",
+    "url": "/static/media/turntable2.c2fa355e.jpg"
+  },
+  {
+    "revision": "6a2d0c9f0f105ca1cd45282bb136267a",
+    "url": "/static/media/turntable3.6a2d0c9f.jpg"
+  },
+  {
+    "revision": "ebdb60f094dc35b9248f23d364becfd2",
+    "url": "/static/media/unliked.ebdb60f0.png"
+  }
+]);
